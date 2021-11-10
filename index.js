b5dd7674b642423a937fcd7bccc01f57242493b9
Lucas Zluhan <lszluhan@hotmail.com>
2021-11-10 19:20:46 -0300

    att

---
+++ index.js
@@ -0,0 +1,26 @@
+// exercicio 1
+
+let numero1 = 2;
+let numero2 = 4;
+
+let media = (numero1 + numero2) / 2;
+
+console.log(`A media no exercicio 1 eh: ${media}`);
+alert(`A media no exercicio 1 eh: ${media}`);
+
+// exercicio 2
+
+let numeroA = 5;
+let numeroB = 12;
+const numeroC = 959746;
+
+let somaDosLet = numeroA + numeroB;
+let quadradoDoConst = numeroC * numeroC;
+let contaDoida = numeroA * numeroB + numeroC;
+
+console.log(`A soma dos dois primeiros é: ${somaDosLet}, O quadrado do terceiro é: ${quadradoDoConst} e o calculo muito doido resulta em: ${contaDoida}`);
+
+function teste() {
+   console.log(teste);
+}
+teste(testando);
